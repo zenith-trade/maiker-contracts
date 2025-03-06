10e1f255a8d2ef74657ed802e59e4899e7e5c606
Kulture <tiziankulture@proton.me>
2025-03-06 16:30:41 +0800

    Add validator and setup scripts

--- configs/validator.cjs
+++ configs/validator.cjs
@@ -17,25 +17,16 @@ module.exports = {
         deployPath: getProgram("maiker_contracts.so"),
       },
       {
-        label: "Dynamic Amm",
-        programId: "Eo7WjKq67rjJQSZxS6z3YkapzY3eMj6Xy8X5EQVn5UaB",
-        deployPath: getProgram("dynamic_amm.so"),
-      },
-      {
-        label: "Vault",
-        programId: "24Uqj9JCLxUeoC3hGfh5W3s9FM9uCHDS2SG3LYwBpyTi",
-        deployPath: getProgram("vault.so"),
+        label: "Meteora DLMM",
+        programId: "LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo",
+        deployPath: getProgram("dlmm.so"),
       },
     ],
     accounts: [{
-      label: "Meteora Config",
-      accountId: "Cqq1zjgSj78BZv5e3HGYkspnVuFgCzuuykckonmT3wmp",
+      label: "Meteora Preset Parameter",
+      accountId: "BYQtcDyv2BoFuf5ghsYDGPA8iX5F4WquK7zCzUsDwJ63",
       cluster: 'https://api.mainnet-beta.solana.com'
-    }, {
-      label: "Native Mint",
-      accountId: "So11111111111111111111111111111111111111112",
-      cluster: 'https://api.mainnet-beta.solana.com'
-    }],
+      }],
     jsonRpcUrl: "localhost",
     websocketUrl: "",
     ledgerDir: "./test-ledger",
